85ee6dd1f984144b6d80ef00bea1b766537eecf5
sotnas1989 <eddyps1989@gmail.com>
2023-11-14 03:08:28 -0500

    Agregados index.html script.js styles.css

---
+++ script.js
@@ -0,0 +1,7 @@
+function formValidation() // In this function we can make more complex validations, but for the example is not needed
+{      
+   // Accessing data by DOM an doing complex validations
+    
+}
+
+document.getElementById("btonAdd").addEventListener("click", formValidation, false);
